1e7ce9c08eee8901441abe0201d7d92c3955798b
Thomas Ralls <tralls.pnw@gmail.com>
2024-03-10 15:20:54 -0700

    test

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,8 @@ const nextConfig = {
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
   basePath: "/nextjs-github-pages",
+  
+  assetPrefix: "/nextjs-github-pages",
 
   /**
    * Disable server-based image optimization. Next.js does not support
